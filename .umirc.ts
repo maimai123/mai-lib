f29e3b5f69dd927d595e46f9aacbf707575eb675
m <15867193547@163.com>
2023-02-26 15:59:47 +0800

    fix: 修改logo

--- .umirc.ts
+++ .umirc.ts
@@ -2,9 +2,8 @@ import { defineConfig } from 'dumi';
 
 export default defineConfig({
   title: 'mai-lib@1.0.0',
-  favicon:
-    'https://prod-saas-5.oss-cn-shanghai.aliyuncs.com/room/05f1fe75836a4fbf83a1ea3d541c3bc7/logo.png',
-  logo: 'https://prod-saas-5.oss-cn-shanghai.aliyuncs.com/room/05f1fe75836a4fbf83a1ea3d541c3bc7/logo.png',
+  favicon: 'http://rqo66mzm0.hd-bkt.clouddn.com/icon.png',
+  logo: 'http://rqo66mzm0.hd-bkt.clouddn.com/icon.png',
   outputPath: 'docs-dist',
   // more config: https://d.umijs.org/config
   // mfsu: {},
